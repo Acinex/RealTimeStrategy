a7a8ae194cf0382b89fc2cdeadc59ce781880742
Mephiztopheles <m.ahrweiler@acinex.com>
2023-05-06 20:34:50 +0200

    Feat: Create URTSGameInstanceSubSystem to transport game information from the lobby to the actual game

---
+++ Source/RealTimeStrategy/Classes/RTSGameInstanceSubSystem.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "RTSGameInstanceSubSystem.generated.h"
+
+class URTSRace;
+/**
+ * 
+ */
+UCLASS()
+class REALTIMESTRATEGY_API URTSGameInstanceSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure)
+	bool GetPlayerColor(const uint8 Index, FLinearColor& Color);
+
+	UFUNCTION(BlueprintPure)
+	bool GetPlayerRace(const uint8 Index, URTSRace*& Race);
+	
+private:
+	UPROPERTY(EditAnywhere, Category="RTS|Players")
+	TArray<FLinearColor> PlayerColors;
+
+	UPROPERTY(EditDefaultsOnly, Category="RTS|Races")
+	TArray<URTSRace*> PlayerRaces;
+};
---
+++ Source/RealTimeStrategy/Private/RTSGameInstanceSubSystem.cpp
@@ -0,0 +1,25 @@
+﻿#include "RTSGameInstanceSubSystem.h"
+
+bool URTSGameInstanceSubSystem::GetPlayerColor(const uint8 Index, FLinearColor& Color)
+{
+	if (!PlayerColors.IsValidIndex(Index))
+	{
+		return false;
+	}
+
+	Color = PlayerColors[Index];
+
+	return true;
+}
+
+bool URTSGameInstanceSubSystem::GetPlayerRace(const uint8 Index, URTSRace*& Race)
+{
+	if (!PlayerRaces.IsValidIndex(Index))
+	{
+		return false;
+	}
+
+	Race = PlayerRaces[Index];
+
+	return true;
+}
